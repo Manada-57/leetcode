0e8d6423fc5e225ab0582ae23e2428f0906c9014
Seetha Raman J <jseetharaman07@gmail.com>
2026-02-27 23:01:55 +0530

    Time: 34 ms (54.84%), Space: 55.4 MB (54.84%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
@@ -3,8 +3,8 @@ class Solution {
     public int minOperations(String s, int k) {
         int n = s.length();
         int initialZeros = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '0') initialZeros++;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0') initialZeros++;
         }
         if (initialZeros == 0) return 0;
         Queue<Integer> queue = new LinkedList<>();
@@ -12,19 +12,28 @@ class Solution {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
         dist[initialZeros] = 0;
+        int[] nextUnvisited = new int[n + 3];
+        for (int i = 0; i < n + 3; i++) nextUnvisited[i] = i;
         while (!queue.isEmpty()) {
             int z = queue.poll();
-            int lowerBound = Math.max(0, k - (n - z));
-            int upperBound = Math.min(k, z);
-            for (int i = lowerBound; i <= upperBound; i++) {
-                int nextZ = z + k - 2 * i;
-                if (nextZ == 0) return dist[z] + 1;
-                if (nextZ >= 0 && nextZ <= n && dist[nextZ] == -1) {
+            int minFlip = Math.max(0, k - (n - z));
+            int maxFlip = Math.min(k, z);
+            int start = z + k - 2 * maxFlip;
+            int end = z + k - 2 * minFlip;
+            for (int nextZ = find(nextUnvisited, start); nextZ <= end; nextZ = find(nextUnvisited, nextZ)) {
+                if (dist[nextZ] == -1) {
                     dist[nextZ] = dist[z] + 1;
+                    if (nextZ == 0) return dist[nextZ];
                     queue.add(nextZ);
                 }
+                nextUnvisited[nextZ] = nextZ + 2;
             }
         }
         return -1;
+    }
+    private int find(int[] parent, int i) {
+        if (i >= parent.length) return i;
+        if (parent[i] == i) return i;
+        return parent[i] = find(parent, parent[i]);
     }
 }
