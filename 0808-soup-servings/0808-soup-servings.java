2ec1d159845acc453d8514f15ac6c7d9b81af685
Seetha Raman J <jseetharaman07@gmail.com>
2026-06-22 22:14:50 +0530

    Time: 0 ms (100%), Space: 45.5 MB (85.9%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.java
@@ -0,0 +1,37 @@
+class Solution {
+    private double[][] memo;
+
+    public double soupServings(int n) {
+        if (n > 4800) {
+            return 1.0;
+        }
+        int servings = (n + 24) / 25;
+        memo = new double[servings + 1][servings + 1];
+        return dfs(servings, servings);
+    }
+
+    private double dfs(int a, int b) {
+        if (a <= 0 && b <= 0) {
+            return 0.5;
+        }
+        if (a <= 0) {
+            return 1.0;
+        }
+        if (b <= 0) {
+            return 0.0;
+        }
+        if (memo[a][b] > 0) {
+            return memo[a][b];
+        }
+        
+        double prob = 0.25 * (
+            dfs(a - 4, b) + 
+            dfs(a - 3, b - 1) + 
+            dfs(a - 2, b - 2) + 
+            dfs(a - 1, b - 3)
+        );
+        
+        memo[a][b] = prob;
+        return prob;
+    }
+}
