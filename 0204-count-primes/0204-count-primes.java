8b398251044d92a9d0f6a209f647bbe4a8e212b8
Seetha Raman J <jseetharaman07@gmail.com>
2026-08-02 10:47:31 +0530

    Time: 91 ms (85.32%), Space: 51.3 MB (22.98%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        boolean[] isComposite = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                for (long j = (long) i * i; j < n; j += i) {
+                    isComposite[(int) j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}
