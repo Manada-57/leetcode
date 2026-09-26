acb381e1d37a049e5cafa95396f7ac15787fda28
Seetha Raman J <jseetharaman07@gmail.com>
2026-09-26 14:33:00 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.96%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[]dp =new int[k+1];
+        int m=0;
+        while(dp[k]<n){
+            m++;
+            for(int i=k;i>=1;i--){
+                dp[i]=dp[i]+dp[i-1]+1;
+            }
+        }
+        return m;
+    }
+}
